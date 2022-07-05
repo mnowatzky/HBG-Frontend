0390e23790267095e2a32fe49ebe54b1726dcd97
Malte Mowatzky <malte.now@gmx.de>
2022-07-05 08:52:33 +0200

    check if lat & long is numeric

--- insert.php
+++ insert.php
@@ -9,19 +9,21 @@ if ($conn->connect_error) {
 
 (string)$lat = $_POST['lat'];
 (string)$long = $_POST['long'];
-$name = $conn->real_escape_string($_POST['name']);
-$city = $conn->real_escape_string($_POST['city']);
-$state = $conn->real_escape_string($_POST['state']);
-$country = $conn->real_escape_string($_POST['country']);
-$name = trim($name);
+if (is_numeric($lat) and is_numeric($long)) {
+    $name = $conn->real_escape_string($_POST['name']);
+    $city = $conn->real_escape_string($_POST['city']);
+    $state = $conn->real_escape_string($_POST['state']);
+    $country = $conn->real_escape_string($_POST['country']);
+    $name = trim($name);
 
-$sql = "INSERT INTO locations (latitude, longitude, name, city, state, country) 
+    $sql = "INSERT INTO locations (latitude, longitude, name, city, state, country) 
                        VALUES ('$lat', '$long', '$name', '$city', '$state', '$country')";
 
-if ($conn->query($sql) === TRUE) {
-    echo "Sticker gespeichert";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    if ($conn->query($sql) === TRUE) {
+        echo "Sticker gespeichert";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
 }
 
 $conn->close();
